e984a7082cd2e7968f8eb6da1c7825eba7e43f9c
paulo.pinto <paulo.pinto98@edu.pucrs.br>
2024-10-24 14:59:32 -0300

    leitura de arquivo

--- src/App.java
+++ src/App.java
@@ -4,29 +4,38 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        String nomeArquivo = "casoc30.txt"; // edita isso aq pra testar os arquivos
+        String nomeArquivo = "caso180_5a.txt"; // edita isso aq pra testar os arquivos
         char[][] matriz = null;
 
         try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
-            String var1;
             String[] linha = br.readLine().split(" ");
-
+            String linha2;
             int x = Integer.parseInt(linha[0]);
             int y = Integer.parseInt(linha[0]);
             matriz= new char[x][y];
             int x1 = 0;
 
-            while ((linha = br.readLine().split(" ")) != null) {
-                for (int y2 = 0; y2 < y; y2++) {
-                    var1 = linha[y2];
-                    matriz[x1][y2] = var1.charAt(0);
+            
+            while ((linha2 = br.readLine()) != null) {
+                int y1=0;
+                for (int y2 = 0; y2 < y*2; y2= y2 + 2) {
+                    matriz[x1][y1] = linha2.charAt(y2);
+                    y1++;
                 }
                 x1++;
             }
+            System.out.println(x + " " + y);
+            for(int x3 = 0 ; x3<matriz.length; x3++){
+                for(int y3 = 0; y3<matriz.length; y3++ ){
+                    System.out.print(matriz[x3][y3]);
+                }
+                System.out.println();
+            }
+        
         } catch (IOException e) {
             System.out.println("Erro ao ler o arquivo: " + e.getMessage());
         } 
-
+        
         
 
 
@@ -34,8 +43,8 @@ public class App {
 
 
 
-        String s = Integer.toBinaryString(0xf);
-        System.out.println(s);
-        System.out.println(s.charAt(0));
+        //String s = Integer.toBinaryString(0xf); ideia de como trabalhar com binario
+        //System.out.println(s);
+        //System.out.println(s.charAt(0));
     }
 }
