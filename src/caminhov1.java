b8eb08438b30df674203bb68b5fdffe7f8bfcc14
paulo.pinto <paulo.pinto98@edu.pucrs.br>
2024-10-24 15:17:06 -0300

    remodelando arquivo caminhov1

--- src/caminhov1.java
+++ src/caminhov1.java
@@ -1,6 +1,31 @@
 import java.util.ArrayList;
 
 public class caminhov1 {
-    private boolean check;
+    private int id;
     private ArrayList<Character> entidades;
+
+    public caminhov1(int id){
+        entidades = new ArrayList();
+        this.id = id;
+    }
+    public void adicionaEntidade(char e){
+        entidades.add(e);
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Character> getEntidades() {
+        return this.entidades;
+    }
+
+    public void setEntidades(ArrayList<Character> entidades) {
+        this.entidades = entidades;
+    }
+
 }
