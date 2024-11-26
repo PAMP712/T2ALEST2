072c0eb1b989c9edb03baf58d867987a94f0def0
Lucas <oliveira.lucas23@edu.pucrs.br>
2024-11-26 00:30:03 -0300

    VERSAO ENTREGUE

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         App app = new App();
-        app.start("caso40_5a.txt");
-        app.start("caso80_5a.txt");
-        app.start("caso100_5a.txt");
-        app.start("caso120_5a.txt");
-        app.start("caso150_5a.txt");
-        app.start("caso180_5a.txt");
-        app.start("caso200_5a.txt");
-        app.start("caso250_5a.txt");
+        
+        medirTempo(app, "caso40_5a.txt");
+        medirTempo(app, "caso80_5a.txt");
+        medirTempo(app, "caso100_5a.txt");
+        medirTempo(app, "caso120_5a.txt");
+        medirTempo(app, "caso150_5a.txt");
+        medirTempo(app, "caso180_5a.txt");
+        medirTempo(app, "caso200_5a.txt");
+        medirTempo(app, "caso250_5a.txt");
+    }
+
+    private static void medirTempo(App app, String arquivo) {
+        long startTime = System.nanoTime();
+        app.start(arquivo);
+        long endTime = System.nanoTime();
+        
+        long duracaoNS = endTime - startTime;
+        double duracaoMS = duracaoNS / 1000000;
+        System.out.printf("Tempo: %.2f ms%n", duracaoMS);
     }
 }
